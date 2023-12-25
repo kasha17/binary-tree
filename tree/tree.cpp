a8b849703ae0d5885b65efd2c9b2fef63da9ae92
kasha17 <145318828+kasha17@users.noreply.github.com>
2023-12-25 09:52:14 +0300

    Update tree.cpp

--- tree/tree.cpp
+++ tree/tree.cpp
@@ -1,11 +1,73 @@
 #include "tree.hpp"
 
-auto tree::insert(int val) -> tree_node * {
-    // Код для добавления элемента в дерево
-    return nullptr;
+tree_node* find_node(tree_node* node, int val) {
+ while (node != nullptr) {
+    if (val == node->value) {
+      return node;
+    } else if (val < node->value) {
+      node = node->left.get();
+    } else {
+      node = node->right.get();
+    }
+ }
+ return nullptr;
+}
+
+auto tree::insert(int val) -> tree_node* {
+ tree_node* node = find_node(root.get(), val);
+ if (node != nullptr) {
+    return node;
+ }
+ tree_node::tree_node* new_node = new tree_node(val);
+ if (!root) {
+    root.reset(new_node);
+    return new_node;
+ }
+ tree_node* parent = nullptr;
+ node = root.get();
+ while (node != nullptr) {
+    parent = node;
+    if (val < node->value) {
+      node = node->left.get();
+    } else {
+      node = node->right.get();
+    }
+ }
+ if (val < parent->value) {
+    parent->left.reset(new_node);
+ } else {
+    parent->right.reset(new_node);
+ }
+ new_node->parent.reset(parent);
+ return new_node;
 }
 
 auto tree::remove(int val) -> bool {
-    // Код для удаления элемента из дерева
+ tree_node *node = find_node(root.get(), val);
+ if (node == nullptr) {
     return false;
+ }
+ tree_node *parent = node->parent;
+ if (node->left != nullptr && node->right != nullptr) {
+    tree_node *successor = node->right;
+    while (successor->left != nullptr) {
+      successor = successor->left;
+    }
+    node->value = successor->value;
+    node = successor;
+ }
+ if (node->left != nullptr) {
+    parent->replace_child(node, std::move(node->left));
+ } else {
+    parent->replace_child(node, std::move(node->right));
+ }
+ delete node;
+ return true;
 }
+
+void replace_child(tree_node *old_child, std::unique_ptr<tree_node> &&new_child) {
+ if (this->left == old_child) {
+    this->left = std::move(new_child);
+ } else {
+    this->right = std::move(new_child);
+ }
